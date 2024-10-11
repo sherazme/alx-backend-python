95f3dc59d2183bb1481f5f200552324383ba21c9
Shiraz Mohammed Elhassan Osman Elmahi <73830525+sherazme@users.noreply.github.com>
2024-10-11 04:46:16 +0200

    python

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" list of floats """
+from typing import Callable, Iterator, Union, Optional, List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Takes list of floats input_list as argument
+    returns their sum as a float
+    """
+
+    return sum(input_list)
